f6d0bca2bb7e90c41beff002e4f5b400c4ff5f6f
Sumner Evans <sumner@beeper.com>
2023-01-27 11:54:11 -0700

    message send log: log timestamp of entry on add
    Signed-off-by: Sumner Evans <sumner@beeper.com>

--- src/main/java/io/finn/signald/MessageSendLogStore.java
+++ src/main/java/io/finn/signald/MessageSendLogStore.java
@@ -10,14 +10,18 @@ package io.finn.signald;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /*
  * Stores the latest 500 messages sent by a given user.
  */
 public class MessageSendLogStore {
+  private static final Logger logger = LogManager.getLogger();
   private List<MessageSendLogEntry> lruMessages = new LinkedList<MessageSendLogEntry>();
 
   public void add(MessageSendLogEntry entry) {
+    logger.debug("adding entry to send log with timestamp {}", entry.getTimestamp());
     lruMessages.add(entry);
     if (lruMessages.size() > 500) {
       lruMessages.remove(0);
